f47b3f838aaa8ccf98a767255e34bb7ab23ab912
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-05-18 16:06:31 -0300

    Delete index-protocol directory

--- index-protocol/utils/contracts/balancer.ts
+++
@@ -1,5 +0,0 @@
-// External Balancer Contracts
-export { BFactory } from "../../typechain/BFactory";
-export { BPool } from "../../typechain/BPool";
-export { BRegistry } from "../../typechain/BRegistry";
-export { ExchangeProxy } from "../../typechain/ExchangeProxy";
--- index-protocol/utils/contracts/kyberV3.ts
+++
@@ -1,5 +0,0 @@
-// External Kyber contracts
-export { KyberNetworkTokenV2 } from "../../typechain/KyberNetworkTokenV2";
-export { DMMFactory } from "../../typechain/DMMFactory";
-export { DMMPool } from "../../typechain/DMMPool";
-export { DMMRouter02 } from "../../typechain/DMMRouter02";
